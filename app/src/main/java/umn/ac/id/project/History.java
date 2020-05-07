414f971dcac967b514e575515d88e3b70caf6b6d
ilyasyura <muhammad.asyura@student.umn.ac.id>
2020-05-07 23:57:53 +0700

    history kelar, payment fix

--- app/src/main/java/umn/ac/id/project/History.java
+++ app/src/main/java/umn/ac/id/project/History.java
@@ -1,20 +1,107 @@
 package umn.ac.id.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class History extends AppCompatActivity {
+    private RecyclerView rView;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    private FirestoreRecyclerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        rView = findViewById(R.id.list);
+        rView.setLayoutManager(new LinearLayoutManager(this));
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        userId = fAuth.getUid();
+        rView = findViewById(R.id.list);
+
+        //Query
+        Query query = fStore.collection("Mahasiswa").document(userId)
+                .collection("Payment History");
+        //RecyclerOption
+        FirestoreRecyclerOptions<HistoryModel> options = new FirestoreRecyclerOptions.Builder<HistoryModel>()
+                .setQuery(query, HistoryModel.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<HistoryModel, HistoryViewHolder>(options) {
+            @NonNull
+            @Override
+            public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_history, parent, false);
+                return new HistoryViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull HistoryViewHolder holder, int position, @NonNull HistoryModel model) {
+                holder.list_name.setText(model.getName());
+                holder.list_price.setText(model.getPrice() + "");
+                holder.list_date.setText(model.getTanggal());
+
+            }
+        };
+        //ViewModel
+        rView.setHasFixedSize(true);
+        rView.setLayoutManager(new LinearLayoutManager(this));
+        rView.setAdapter(adapter);
     }
+
+
 
     @Override
     public void finish() {
         super.finish();
         //overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    private class HistoryViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView list_name;
+        private TextView list_price;
+        private TextView list_date;
+
+        public HistoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            list_name = itemView.findViewById(R.id.list_name);
+            list_price = itemView.findViewById(R.id.list_price);
+            list_date = itemView.findViewById(R.id.list_date);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 }
